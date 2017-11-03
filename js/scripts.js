ae296355836bfe2ed69aa4529c22e5e1ae46fb6c
thatcat13 <thatcat13@gmail.com>
2017-11-03 09:58:14 -0700

    continue to figure out how to format OOP for this pizza app

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,39 @@
 //business logic
 
-// $("input:checkbox[name=toppings]:checked").each(function() {
-//   var yourToppings = $(this).val();
+
 // });
+
+function Pizza(size, toppings, price) {
+  this.size = size;
+  this.toppings = [];
+  this.price = 8;
+  this.priceCalc()
+}
+
+Pizza.prototype.priceCalc = function() {
+  if (this.size === "Small") {
+    this.price = this.price;
+  } else if (this.size === "Medium") {
+    this.price += 3;
+  } else if (this.size === "Large") {
+    this.price += 6;
+  }
+};
+
+
+//frontend logic:
+
+$(document).ready(function(){
+  $("form#sizeSelect").submit(function(event) {
+    event.preventDefault();
+
+    var size = $("select#sizes").val();
+
+  }); //event size
+    alert("this works");
+
+  $("input:checkbox[name=toppings]:checked").each(function() {
+    var yourToppings = $(this).val();
+
+  }); //event toppings
+}); //ready
