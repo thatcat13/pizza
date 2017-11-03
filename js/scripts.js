72e4919dc98fa30c31665d215cbf43fb34337e04
thatcat13 <thatcat13@gmail.com>
2017-11-03 12:12:37 -0700

    build out backend logic using object and custom method but frontend giving no output at all yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,40 @@
 //business logic
-
-
-// });
-
-function Pizza(size, toppings) {
-  this.price = 8;
+function Pizza(size, toppings, price) {
   this.size = size;
   this.toppings = [0];
+  this.price = 8;
   this.sizeCalc();
 }
 
-Pizza.prototype.sizeCalc = function(size) {
+Pizza.prototype.sizeCalc = function() {
   if (this.size === "Small") {
-    this.price === this.price;
+    this.price = 8;
   } else if (this.size === "Medium") {
     this.price += 3;
   } else if (this.size === "Large") {
     this.price += 6;
   }
-}
-
-
-finalPrice = this.toppings + this.price;
-
+};
 
 function sum(total, number) {
     return total + number;
-};
-
+}
 
 //frontend logic:
 $(document).ready(function(){
-  var yourTotal;
   $("#pizzaTotal").submit(function(event) {
     event.preventDefault();
 
     var size = $("select#sizes").val();
 
+    var newPizza = Pizza.sizeCalc(size);
+
     $("input:checkbox[name=toppings]:checked").each(function() {
       var yourToppings = parseInt($(this).val());
+      newPizza.toppings.push(yourToppings.reduce(sum, 0));
+    });//checkbox toppings array
 
-      toppings.push(yourToppings.reduce(sum, 0));
-    });
-
-    console.log(yourToppings);
-
-    yourTotal = new Pizza(size, yourToppings, price);
+    $("#total-cost").text("Your total cost is: " + newPizza());
   }); //event
 
 }); //ready
