e559a49f30996709e96056b2c301121036b527aa
thatcat13 <thatcat13@gmail.com>
2017-11-03 11:25:30 -0700

    continue to work on business logic by creating a pizza object that will receive input from jquery and calculate using custom sizeCalc method

--- js/scripts.js
+++ js/scripts.js
@@ -3,36 +3,49 @@
 
 // });
 
-function Pizza(size, toppings, price) {
-  this.size = size;
-  this.toppings = [];
+function Pizza(size, toppings) {
   this.price = 8;
-  this.priceCalc()
+  this.size = size;
+  this.toppings = [0];
+  this.sizeCalc();
 }
 
-Pizza.prototype.priceCalc = function() {
+Pizza.prototype.sizeCalc = function(size) {
   if (this.size === "Small") {
-    this.price = this.price;
+    this.price === this.price;
   } else if (this.size === "Medium") {
     this.price += 3;
   } else if (this.size === "Large") {
     this.price += 6;
   }
+}
+
+
+finalPrice = this.toppings + this.price;
+
+
+function sum(total, number) {
+    return total + number;
 };
 
 
 //frontend logic:
 $(document).ready(function(){
-  $("form#sizeSelect").submit(function(event) {
+  var yourTotal;
+  $("#pizzaTotal").submit(function(event) {
     event.preventDefault();
 
     var size = $("select#sizes").val();
 
-  }); //event size
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      var yourToppings = parseInt($(this).val());
 
+      toppings.push(yourToppings.reduce(sum, 0));
+    });
 
-  $("input:checkbox[name=toppings]:checked").each(function() {
-    var yourToppings = $(this).val();
+    console.log(yourToppings);
 
-  }); //event toppings
+    yourTotal = new Pizza(size, yourToppings, price);
+  }); //event
+
 }); //ready
