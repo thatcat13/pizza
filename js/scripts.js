1d47e09e81d8bf5a3eb57e60298b7b763ba62a1e
thatcat13 <thatcat13@gmail.com>
2017-11-03 15:41:12 -0700

    array value for toppings is not coming from object operations but jquery

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ function sum(price, toppings) {
     return price + toppings;
 };
 
-Pizza.prototype.sizeCalc = function(size) {
+Pizza.prototype.sizeCalc = function() {
   if (this.size === "Small") {
     this.price === 8;
   } else if (this.size === "Medium") {
@@ -20,26 +20,34 @@ Pizza.prototype.sizeCalc = function(size) {
   }
 };
 
-pizza = new Pizza();
+Pizza.prototype.grandTotal = function() {
+  return this.price + this.toppings;
+}
+
+Pizza.prototype.outputMessage = function() {
+  return "Your total is: " + this.price;
+}
 
 //frontend logic:
 $(document).ready(function(){
   $("form#yourPizza").submit(function(event) {
     event.preventDefault();
 
-    var inputtedSize = $("select#size").val();
-    var newPizza = new Pizza(inputtedSize);
-    console.log(newPizza);
+    var size = $("select#size").val();
 
     var yourToppings = $("input:checkbox[name=toppings]:checked").map(function() {
       return parseInt($(this).val());
     }).get();
-      // yourToppings.reduce(sum, 0);
 
-    newPizza.toppings.push(yourToppings.reduce(sum, 0));
+    var newPizza = new Pizza(size, yourToppings);
     console.log(newPizza);
 
+    // newPizza.toppings.push(yourToppings.reduce(sum, 0));
+    // console.log(newPizza);
+
   }); //event toppings
+
+  // console.log(newPizza.grandTotal());
   //
   //   $("#total-cost").text("Your total cost is: " + newPizza());
 
