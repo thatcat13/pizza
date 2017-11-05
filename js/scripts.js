aebc8d778e852bddb2877c8c5251820e4cffd711
thatcat13 <thatcat13@gmail.com>
2017-11-05 12:22:57 -0800

    get both pizza and toppings object to return the values of their respective properties using a custom method for each

--- js/scripts.js
+++ js/scripts.js
@@ -2,49 +2,24 @@
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = [];
-  this.price;//placeholder
-  // this.sizeCalc();
 }
 
-function Toppings(toppings) {
-  this.toppings = toppings;
+function Toppings(allToppings) {
+  this.allToppings = allToppings;
 }
 
 function sum(a, b) {
     return a + b;
 };
 
-// Pizza.prototype.sizeCalc = function() {
-//   if (this.size === "Small") {
-//     this.price === 8;
-//   } else if (this.size === "Medium") {
-//     this.price === 11;
-//   } else if (this.size === "Large") {
-//     this.price === 14;
-//   }
-// };
-
-// Pizza.prototype.total = function(toppings) {
-//   var price = 0;
-//   var total = 0;
-//
-//   for (var i = 0; i < this.toppings.length; i++) {
-//     this.price += toppings[i].this.toppings;
-//   }
-//
-//   total = this.price;
-//
-//   return total;
-//
-// };
-
-Pizza.prototype.grandTotal = function() {
-  return this.size + this.toppings;
+Pizza.prototype.sizeTotal = function() {
+  return this.size;
 }
 
-// Pizza.prototype.outputMessage = function() {
-//   return "Your total is: " + Pizza.grandTotal;
-// }
+Toppings.prototype.toppingsTotal = function() {
+  return this.allToppings;
+}
+
 
 //frontend logic:
 $(document).ready(function(){
@@ -58,50 +33,10 @@ $(document).ready(function(){
     }).get();
 
     var newToppings = new Toppings(inputtedToppings.reduce(sum, 0));
-    // console.log(inputtedToppings.reduce(sum, 0));
     var newPizza = new Pizza(size, toppings);
     newPizza.toppings.push(newToppings);
-    console.log(newPizza);
 
+    $("#total-cost").text("Your total cost is: " + (newToppings.toppingsTotal() + newPizza.sizeTotal()) + " dollars");
 
-        //
-        // $("span#rollTotal").text(currentRoll.reduce(add, 0))
-
-
-    // newToppings.push = new Toppings(inputtedVeggie)
-    //   console.log(newToppings);
-    // var newPizza = new Pizza(size, toppings);
-
-
-
-  // });
-    // var inputtedVeggie = $("input:checkbox[name=veggie]:checked").map(function() {
-    //   return parseInt($(this).val());
-    // }
-
-
-    // var newToppings = new Toppings(inputtedStreet, inputtedCity, inputtedState);
-    // newContact.addresses.push(newAddress);
-
-
-
-
-
-    // var yourToppings = $("input:checkbox[name=toppings]:checked").map(function() {
-    //   return parseInt($(this).val());
-    // }).get();
-    //
-    // var newPizza = new Pizza(size);
-    // console.log(newPizza);
-
-    // newPizza.toppings.push(yourToppings.reduce(sum, 0));
-    // console.log(newPizza);
-
-  }); //event toppings
-
-  // console.log(newPizza.grandTotal());
-  //
-  //   $("#total-cost").text("Your total cost is: " + newPizza());
-
-
+  }); //event yourPizza
 }); //ready
